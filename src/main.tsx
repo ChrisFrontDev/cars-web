b238e069ca279fdbbc08999d0c3ee1b8eeeaedc9
Christian Santos <christiansantos.dev@gmail.com>
2024-01-24 20:55:08 -0300

    feat: add react-query setup

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,14 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 import { Box, Container, CssBaseline, ThemeProvider } from '@mui/material';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
 import { theme } from './theme.ts';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -17,7 +20,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <CssBaseline />
       <Container maxWidth="lg">
         <Box sx={{ my: 4 }}>
-          <App />
+          <QueryClientProvider client={queryClient}>
+            <App />
+          </QueryClientProvider>
         </Box>
       </Container>
     </ThemeProvider>
