182212eaf80b86248b0e10e49b26d62eacb82923
Christian Santos <christiansantos.dev@gmail.com>
2024-01-24 20:01:55 -0300

    chore: Add zod package dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter } from 'react-router-dom';
 
-import { AppRoutes } from "./routes"
+import { AppRoutes } from './routes';
 
 export default function App() {
   return (
     <BrowserRouter>
       <AppRoutes />
     </BrowserRouter>
-  )
+  );
 }
 
 //  TODO: Add a new component to display a list of cars
 // TODO: Add a new component to display a single car
-// TODO: DARK MODE  https://mui.com/customization/palette/#dark-mode
+// TODO: DARK MODE/LIGHT MODE  https://mui.com/customization/palette/#dark-mode
 //TODO: ADD ROUTING https://mui.com/components/app-bar/#app-bar-with-a-primary-search-field
 
 //TODO: ADD A FORM TO ADD A NEW CAR
