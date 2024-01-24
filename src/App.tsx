0bfacb2ed4b4ed57e710bcef9ef6f94ef671b2e0
Christian Santos <christiansantos.dev@gmail.com>
2024-01-24 08:57:05 -0300

    chore: eslint + prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>hello world</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+//  TODO: Add a new component to display a list of cars
+// TODO: Add a new component to display a single car
+// TODO: DARK MODE  https://mui.com/customization/palette/#dark-mode
+//TODO: ADD ROUTING https://mui.com/components/app-bar/#app-bar-with-a-primary-search-field
+
+//TODO: ADD A FORM TO ADD A NEW CAR
