9ee112947662887177f6608cd91149b498179454
Christian Santos <christiansantos.dev@gmail.com>
2024-01-25 02:57:12 -0300

    feat: add create car modal

--- src/features/list-cars/list-cars.tsx
+++ src/features/list-cars/list-cars.tsx
@@ -1,25 +1,27 @@
 import { PersonAddAlt } from '@mui/icons-material';
 import { Button, Paper } from '@mui/material';
-import { Link as RouterLink } from 'react-router-dom';
+import { useState } from 'react';
 
 import BasePageLayout from '../../layouts/base-page-layout';
+import CreateCarModal from '../create-car/create';
 import CarGrid from './components/car-grid';
 import { useGetCars } from './hooks/useGetCars';
+//TODO:  error remove error type handling from component this violate the single responsibility principle
 
-const List = () => {
+const ListCar = () => {
   const { cars, isLoading, error } = useGetCars();
+  const [isModalOpen, setModalOpen] = useState(false);
 
   if (isLoading) return <div>Loading...</div>;
-
-  //TODO:  error remove error type handling from component this violate the single responsibility principle
   if (error instanceof Error) return <div>An error occurred: {error.message}</div>;
   if (!cars) return <div>No cars available.</div>;
 
   return (
     <BasePageLayout pageTitle="Cars" path={[{ label: 'Cars', to: '/' }, { label: 'List' }]}>
-      <Button component={RouterLink} to="/users/new" variant="contained" startIcon={<PersonAddAlt />}>
+      <Button variant="contained" startIcon={<PersonAddAlt />} onClick={() => setModalOpen(true)}>
         New Car
       </Button>
+      <CreateCarModal open={isModalOpen} onClose={() => setModalOpen(false)} />
       <Paper sx={{ mt: 2 }}>
         <CarGrid items={cars} />
       </Paper>
@@ -27,4 +29,4 @@ const List = () => {
   );
 };
 
-export default List;
+export default ListCar;
